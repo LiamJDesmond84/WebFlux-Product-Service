a1af735ba49dcc2a2032b0816ab49f010b5262ed
Liam Desmond <liamjdesmond84@gmail.com>
2022-12-17 18:19:31 -0600

    Adding Product entity

--- src/main/java/com/liam/productservice/entities/Product.java
+++ src/main/java/com/liam/productservice/entities/Product.java
@@ -2,11 +2,13 @@ package com.liam.productservice.entities;
 
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
 
 @Data
 @ToString
 public class Product {
 	
+	@Id
 	private String Id;
 	
 	private String description;
