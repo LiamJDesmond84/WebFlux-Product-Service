9808b5f7b4e39e964b07fc82004eeeaae951af65
Liam Desmond <liamjdesmond84@gmail.com>
2022-12-17 18:22:53 -0600

    Adding ProductRepository

--- src/main/java/com/liam/productservice/repositories/ProductRepository.java
+++ src/main/java/com/liam/productservice/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.liam.productservice.repositories;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.liam.productservice.entities.Product;
 
+@Repository
 public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
 
 }
