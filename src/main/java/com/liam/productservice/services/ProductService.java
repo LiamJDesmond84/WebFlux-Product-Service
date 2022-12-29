c96692089755aa4efd02aab1f92874e900a89c4e
Liam Desmond <liamjdesmond84@gmail.com>
2022-12-28 18:28:44 -0600

    Updating ProductService

--- src/main/java/com/liam/productservice/services/ProductService.java
+++ src/main/java/com/liam/productservice/services/ProductService.java
@@ -1,8 +1,24 @@
 package com.liam.productservice.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.liam.productservice.dtos.ProductDto;
+import com.liam.productservice.repositories.ProductRepository;
+import com.liam.productservice.util.EntityDTOUtil;
+
+import reactor.core.publisher.Flux;
 
 @Service
 public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public Flux<ProductDto> getAllProducts() {
+		
+		return productRepository.findAll()
+				.map(EntityDTOUtil.toDto(return x -> x));
+	}
 
 }
