012bb0812e45a96b1b55024cc76cd7073b662730
Liam Desmond <liamjdesmond84@gmail.com>
2023-01-04 21:27:06 -0600

    Adding ProductController

--- src/main/java/com/liam/productservice/controllers/ProductController.java
+++ src/main/java/com/liam/productservice/controllers/ProductController.java
@@ -1,14 +1,29 @@
 package com.liam.productservice.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.liam.productservice.dtos.ProductDto;
 
 import com.liam.productservice.services.ProductService;
 
+import reactor.core.publisher.Flux;
+
 @RestController
+@RequestMapping("product")
 public class ProductController {
 
 	
 	@Autowired
 	private ProductService productService;
+	
+	
+	@GetMapping("all")
+	public Flux<ProductDto> getAllProducts() {
+		
+		return productService.getAllProducts();
+	}
+	
 }
