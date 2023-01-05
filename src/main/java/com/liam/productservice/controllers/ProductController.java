b4db503a2b9cb16520902b947fe48d0b594b5ad0
Liam Desmond <liamjdesmond84@gmail.com>
2023-01-04 21:25:03 -0600

    Adding ProductController

--- src/main/java/com/liam/productservice/controllers/ProductController.java
+++ src/main/java/com/liam/productservice/controllers/ProductController.java
@@ -1,5 +1,14 @@
 package com.liam.productservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.liam.productservice.services.ProductService;
+
+@RestController
 public class ProductController {
 
+	
+	@Autowired
+	private ProductService productService;
 }
