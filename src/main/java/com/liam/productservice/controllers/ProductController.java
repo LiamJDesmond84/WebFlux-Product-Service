701450941acb8c38f41f155ef688b5cf06c0b533
Liam Desmond <liamjdesmond84@gmail.com>
2023-01-12 18:33:19 -0600

    Updating service

--- src/main/java/com/liam/productservice/controllers/ProductController.java
+++ src/main/java/com/liam/productservice/controllers/ProductController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,7 +35,7 @@ public class ProductController {
 	
 	
 	@GetMapping("{id}")
-	public Mono<ResponseEntity<ProductDto>> getOneProduct(String id) {
+	public Mono<ResponseEntity<ProductDto>> getOneProduct(@PathVariable("id") String id) {
 		
 		return productService.getProductById(id)
 				.map(ResponseEntity::ok)
@@ -41,7 +43,7 @@ public class ProductController {
 	}
 	
 	@PostMapping("newProduct")
-	public Mono<ProductDto> createProduct(Mono<ProductDto> productDto) {
+	public Mono<ProductDto> createProduct(@RequestBody Mono<ProductDto> productDto) {
 		
 		return productService.insertNewProduct(productDto);
 	}
