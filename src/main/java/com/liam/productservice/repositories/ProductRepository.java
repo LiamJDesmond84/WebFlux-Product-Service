19d679295518dc08e142de56aca46c4d21034c77
Liam Desmond <liamjdesmond84@gmail.com>
2023-01-26 19:48:00 -0600

    Updating Repository

--- src/main/java/com/liam/productservice/repositories/ProductRepository.java
+++ src/main/java/com/liam/productservice/repositories/ProductRepository.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import com.liam.productservice.entities.Product;
 
+import reactor.core.publisher.Flux;
+
 @Repository
 public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
+	
+	Flux<Product> findByPriceBetween(int min, int max);
 
 }
